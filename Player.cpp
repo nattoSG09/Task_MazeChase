fe0408522c9a80667d29a388361c9641f11ccb55
颯 鈴木 <220185@jc-21.jp>
2023-02-15 15:06:03 +0900

    いろいろ追加part1

---
+++ Player.cpp
@@ -0,0 +1,27 @@
+#include "Player.h"
+
+//コンストラクタ
+Player::Player(GameObject* parent)
+	: GameObject(parent, "Player")
+{
+}
+
+//初期化
+void Player::Initialize()
+{
+}
+
+//更新
+void Player::Update()
+{
+}
+
+//描画
+void Player::Draw()
+{
+}
+
+//開放
+void Player::Release()
+{
+}
