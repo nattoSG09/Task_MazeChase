87c393b1678265dc731935dcd5f1776f3f3a0ba1
颯 鈴木 <220185@jc-21.jp>
2023-02-15 13:32:15 +0900

    カメラ関連のクラス追加

---
+++ Camera.cpp
@@ -0,0 +1,27 @@
+#include "Camera.h"
+
+//コンストラクタ
+Camera::Camera(GameObject* parent)
+	: GameObject(parent, "Camera")
+{
+}
+
+//初期化
+void Camera::Initialize()
+{
+}
+
+//更新
+void Camera::Update()
+{
+}
+
+//描画
+void Camera::Draw()
+{
+}
+
+//開放
+void Camera::Release()
+{
+}
