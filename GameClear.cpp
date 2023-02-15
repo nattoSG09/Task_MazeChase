1f1e282e22c74a3e8e7570b696d4ca3bcec114a4
颯 鈴木 <220185@jc-21.jp>
2023-02-15 12:45:19 +0900

    aaa

---
+++ GameClear.cpp
@@ -0,0 +1,27 @@
+#include "GameClear.h"
+
+//コンストラクタ
+GameClear::GameClear(GameObject* parent)
+	: GameObject(parent, "GameClear")
+{
+}
+
+//初期化
+void GameClear::Initialize()
+{
+}
+
+//更新
+void GameClear::Update()
+{
+}
+
+//描画
+void GameClear::Draw()
+{
+}
+
+//開放
+void GameClear::Release()
+{
+}
---
+++ GameOver.cpp
@@ -0,0 +1,27 @@
+#include "GameOver.h"
+
+//コンストラクタ
+GameOver::GameOver(GameObject* parent)
+	: GameObject(parent, "GameOver")
+{
+}
+
+//初期化
+void GameOver::Initialize()
+{
+}
+
+//更新
+void GameOver::Update()
+{
+}
+
+//描画
+void GameOver::Draw()
+{
+}
+
+//開放
+void GameOver::Release()
+{
+}
